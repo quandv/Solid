d3f4cbc5ee6620bf8654601ee6bc8e4e97059f7c
zujs <doanvietquan.dev@gmail.com>
2023-01-06 11:03:50 +0700

    solid: open-closed not violate

--- src/modules/delivery/delivery.service.ts
+++ src/modules/delivery/delivery.service.ts
@@ -1,10 +1,24 @@
 import { IDeliveryProvider, IDeliveryService } from "./delivery.interface";
 
 export class DeliveryService implements IDeliveryService {
-  private readonly deliveryProvider: IDeliveryProvider
+  readonly deliveryProvider: IDeliveryProvider
 
   constructor(deliveryProvider: IDeliveryProvider) {
     this.deliveryProvider = deliveryProvider;
+  }
+
+  createOrder = async (order) => {
+    if (order.weight > 100) {
+      return await this.deliveryProvider.createVietelOrder(order);
+    } else {
+      return await this.deliveryProvider.createGHNOrder(order);
+    }
+  }
+}
+
+export class DeliveryV2Service extends DeliveryService {
+  constructor(deliveryProvider: IDeliveryProvider) {
+    super(deliveryProvider);
   }
 
   createOrder = async (order) => {
