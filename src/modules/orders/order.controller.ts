985a7e8c6c635cee1639572064df55c72cb9927e
zujs <doanvietquan.dev@gmail.com>
2023-01-06 15:52:21 +0700

    solid: dependency inversion violate

--- src/modules/orders/order.controller.ts
+++ src/modules/orders/order.controller.ts
@@ -1,15 +1,7 @@
-import { IOrderService } from "./order.interface"
 import { OrderService } from "./order.service";
 
 export class OrderController {
   private orderService: OrderService = new OrderService();
-  // orderService: OrderService;
-  // constructor(
-  //   orderService: OrderService,
-  // ){
-  //   this.orderService = orderService;
-  //   this.name = "haha";
-  // }
 
   createOrder = (req, res, next) => {
     res.status(201).json({
